90aeff1c7a8c11e455212cddc0c2eefefaf9c31d
Wolfie Waffle <wolfie.waffle11@gmail.com>
2017-05-30 20:14:32 -0600

    First commit

---
+++ Assets/ClockAnimator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public Transform hours, minutes, seconds;
+
+	private const float
+		hoursToDegrees = 360 / 12f,
+		minutesToDegrees = 360f / 60f,
+		secondsToDegrees = 360f / 60f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime time = DateTime.Now;
+		hours.localRotation = 
+			Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+		minutes.localRotation = 
+			Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+		seconds.localRotation = 
+			Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+	}
+}
